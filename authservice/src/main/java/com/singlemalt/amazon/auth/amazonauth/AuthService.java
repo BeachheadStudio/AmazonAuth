6d8b76199d94ab766abb9265d28e596b49b55e92
kmiller <keith.miller@activision.com>
2016-04-05 10:29:25 -0700

    Changing name of game object

--- authservice/src/main/java/com/singlemalt/amazon/auth/amazonauth/AuthService.java
+++ authservice/src/main/java/com/singlemalt/amazon/auth/amazonauth/AuthService.java
@@ -219,7 +219,7 @@ public class AuthService implements AuthorizationListener, AmazonGamesCallback,
         if(this.playerId != null) {
             if(!this.playerId.equals(playerId)) {
                 this.playerId = playerId;
-                UnityPlayer.UnitySendMessage("Main Camera", "PlayerChange", "true");
+                UnityPlayer.UnitySendMessage("AuthGameObject", "PlayerChange", "true");
             }
         } else {
             this.playerId = playerId;
@@ -254,19 +254,19 @@ public class AuthService implements AuthorizationListener, AmazonGamesCallback,
             anonymous = false;
 
             Log.d(TAG, "login success");
-            UnityPlayer.UnitySendMessage("Main Camera", "LoginResult", Status.Success.toString());
+            UnityPlayer.UnitySendMessage("AuthGameObject", "LoginResult", Status.Success.toString());
         } else if(loginStatus.equals(Status.Cancel)) {
             anonymous = true;
             player = null;
 
             Log.d(TAG, "login cancelled");
-            UnityPlayer.UnitySendMessage("Main Camera", "LoginResult", Status.Cancel.toString());
+            UnityPlayer.UnitySendMessage("AuthGameObject", "LoginResult", Status.Cancel.toString());
         } else if(loginStatus.equals(Status.Failure) || gcStatus.equals(Status.Failure)) {
             anonymous = true;
             player = null;
 
             Log.d(TAG, "login failed");
-            UnityPlayer.UnitySendMessage("Main Camera", "LoginResult", Status.Failure.toString());
+            UnityPlayer.UnitySendMessage("AuthGameObject", "LoginResult", Status.Failure.toString());
         }
     }
 
